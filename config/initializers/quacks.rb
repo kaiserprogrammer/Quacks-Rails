c593ce8247caa2cc338b5357678d581b9398c596
Jürgen Bickert <juergenbickert@gmail.com>
2012-03-27 22:08:39 +0200

    no need to abstract from datamapper here

--- config/initializers/quacks.rb
+++ config/initializers/quacks.rb
@@ -6,7 +6,7 @@ require_relative "../../app/views/users/users_view"
 require_relative "../../app/views/users/user_view"
 
 db = YAML.load(File.new(Rails.root + "config/database.yml"))
-DB.setup(:default, db[Rails.env])
-DB.auto_upgrade!
+DataMapper.setup(:default, db[Rails.env])
+DataMapper.auto_upgrade!
 
 IMG_SIZE = 170
