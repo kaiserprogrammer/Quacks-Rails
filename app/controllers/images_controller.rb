2fb62748ebc32cd1039dd84ba1d8f2435ab87e5b
Jürgen Bickert <juergenbickert@gmail.com>
2012-04-11 02:37:33 +0200

    don't change images when user is not logged in

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -4,6 +4,8 @@ class ImagesController < ApplicationController
   end
 
   def update
+    redirect_to author_path(author_id) unless user_id
+
     author_id = params[:id].to_i
     url = params[:url]
     t = AddImage.new(author_id, url, DB.new)
